e67873cff63c0d9db52330e503dcbf07baecda0d
Đặng Mai Anh Tú <dangmaianhtu@gmail.com>
2026-01-30 17:37:52 +0700

    Revert "update swagger UI config"
    This reverts commit 4df87da844a69fdac2db47813593bbb5ad48f1ad.

--- sma-core-service/src/main/java/com/sma/core/config/OpenApiConfig.java
+++ sma-core-service/src/main/java/com/sma/core/config/OpenApiConfig.java
@@ -4,23 +4,38 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
+
+    @Value("${springdoc.swagger-ui.server-url}")
+    private String swaggerServerUrl;
 
     private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
     public OpenAPI customOpenAPI() {
+        // Define server
+        Server apiServer = new Server();
+        apiServer.setUrl(swaggerServerUrl);
+        apiServer.setDescription("API Server");
+
         // Define security scheme
         SecurityScheme bearerAuthScheme = new SecurityScheme()
                 .name(SECURITY_SCHEME_NAME)
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT");
+
+        // Apply security scheme globally
         return new OpenAPI()
+                .servers(List.of(apiServer))
                 .components(new Components().addSecuritySchemes(SECURITY_SCHEME_NAME, bearerAuthScheme))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
     }
