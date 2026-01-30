240ded871b2d1ed2ba7a19550dbc551450b68d79
Đặng Mai Anh Tú <dangmaianhtu@gmail.com>
2026-01-30 10:46:29 +0700

    init clean backend repo

---
+++ sma-core-service/src/main/java/com/sma/core/SmaCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sma.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmaCoreServiceApplication.class, args);
+    }
+
+}
