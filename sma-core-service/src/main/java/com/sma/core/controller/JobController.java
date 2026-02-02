9b910606d8e66fefa635db0a12a6e05a4fa97ca5
haocp <haocpse184107@fpt.edu.vn>
2026-02-02 19:29:44 +0700

    [SMA133] add job service and controller

---
+++ sma-core-service/src/main/java/com/sma/core/controller/JobController.java
@@ -0,0 +1,15 @@
+package com.sma.core.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequestMapping("/v1/jobs")
+public class JobController {
+}
