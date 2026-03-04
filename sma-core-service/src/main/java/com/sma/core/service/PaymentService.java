1520d5c94d45e37741963a6e4155d772ca89dd5c
haocp <haocpse184107@fpt.edu.vn>
2026-03-04 19:37:38 +0700

    [SMA-150] enhance create subscription for payment

--- sma-core-service/src/main/java/com/sma/core/service/PaymentService.java
+++ sma-core-service/src/main/java/com/sma/core/service/PaymentService.java
@@ -1,12 +1,15 @@
 package com.sma.core.service;
 
 import com.sma.core.dto.request.payment.SePayWebhookRequest;
+import com.sma.core.dto.response.payment.CreatePaymentResponse;
 import com.sma.core.entity.Subscription;
 import com.sma.core.enums.PaymentMethod;
+import com.sma.core.enums.PaymentStatus;
 
 public interface PaymentService {
 
-    String createQR(Subscription subscription, PaymentMethod method);
+    CreatePaymentResponse createQR(Subscription subscription, PaymentMethod method);
     Boolean confirm(String authorization, SePayWebhookRequest request);
+    PaymentStatus getPaymentStatus(Integer id);
 
 }
